b72ba21c6558afe8c818bda43e2c6531adfd4def
BarrakBM <barrak_mandani@live.com>
2025-02-11 20:22:00 +0300

    6th task completed

---
+++ Functions/src/survival.kt
@@ -0,0 +1,9 @@
+fun survivalChance(supplies: Int){
+    var survivalChance = supplies * 10
+    println("Survival Chance: $survivalChance")
+}
+
+fun main(){
+    // 6th task
+    survivalChance(8)
+}
