9c3aed942ce35c598cda5ed3b59d65671705a258
BarrakBM <barrak_mandani@live.com>
2025-02-11 20:20:12 +0300

    4th taks completed

---
+++ Functions/src/camel.kt
@@ -0,0 +1,12 @@
+fun startCamelRide(time: Int, speed: Int = 5){
+    var distance: Int = time/speed
+    println("Camel Ride: Distance = $distance km, Speed = $speed km/h, Time = $time hours")
+
+}
+
+fun main(){
+
+    // 4th task
+    startCamelRide(50,7)
+    startCamelRide(50, )
+}
