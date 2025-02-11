ad849d8fcfce201319b2226482f4bdcea088f7a5
BarrakBM <barrak_mandani@live.com>
2025-02-11 20:19:39 +0300

    3rd task completed

---
+++ Functions/src/temp.kt
@@ -0,0 +1,10 @@
+fun currentTemperature(morningTemperature: Int, afternoonTemperature: Int){
+    var avgTemp = (morningTemperature + afternoonTemperature)/2;
+    println("Average Temperature: $avgTemp")
+
+}
+fun main(){
+    // 3rd task
+    currentTemperature(25, 40)
+
+}
