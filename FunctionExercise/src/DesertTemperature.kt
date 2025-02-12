bad3ddeeccf6da81045b562ae3309b526bf928c8
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-12 17:37:42 +0300

    average temperature

---
+++ FunctionExercise/src/DesertTemperature.kt
@@ -0,0 +1,8 @@
+fun currentTemperature(morningTemperature: Int, afternoonTemperature: Int): Int {
+    return (morningTemperature + afternoonTemperature) / 2
+}
+
+fun main() {
+    val averageTemp = currentTemperature(25, 40)
+    println("Average temperature: $averageTemp")
+}
