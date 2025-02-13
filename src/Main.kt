f48895819866b49785cc6432178e83676b21f417
miskaldukhi <miskaldukhi@gmail.com>
2025-02-13 12:33:49 +0300

    functions part1 done

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,10 @@
-fun greetTraveler() {
-    println("Welcome to the Desert, Traveler!")
-}
-
-fun main() {
-    greetTraveler()
-}
-fun findOasis(x:Int, y:Int):String {
-    findOasis(x: Int, y:Int)
-    return "Oasis found at the coordinates ($x, $y)"
-    println(findOasis(10, 20))
+fun greetTraveler()
+    {
+        println("Welcome to the Desert, Traveler!")
+    }
 
 
-}
+    fun main() {
+        greetTraveler()
+
+    }
